0b21be198ef34080925ebd61554bbe93396fe690
cmcoke <minoru.cm@gmail.com>
2019-06-28 20:27:40 -0500

    used media queries to make website responsive

--- app/source/scripts/app.js
+++ app/source/scripts/app.js
@@ -1,1 +1,12 @@
 
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    prevNextButtons: false,
+    pageDots: false,
+    wrapAround: true,
+    imagesLoaded: true,
+    autoPlay: 6000,
+});
