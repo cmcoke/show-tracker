de0b59ce77ac9610347abf8e6f8227c620ec566b
cmcoke <minoru.cm@gmail.com>
2019-06-28 20:44:54 -0500

    used JQuery to add smooth scrolling to nav links

--- app/source/scripts/app.js
+++ app/source/scripts/app.js
@@ -1,3 +1,4 @@
+// Flickity Carousel Config
 
 var elem = document.querySelector('.carousel');
 var flkty = new Flickity(elem, {
@@ -10,3 +11,17 @@ var flkty = new Flickity(elem, {
     imagesLoaded: true,
     autoPlay: 6000,
 });
+
+// Smooth Scroll
+$(document).ready(function() {
+
+    var scrollLink = $('.scroll');
+  
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top - 100
+      }, 1000);
+    });
+  
+})
